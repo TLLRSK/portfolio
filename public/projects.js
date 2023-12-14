65ecb0222c0154f2e485efd3706fa81498b8ff9d
TLLRSK <tullarisk@gmail.com>
2023-12-14 09:36:57 +0100

    adding projects description

--- public/projects.js
+++ public/projects.js
@@ -2,37 +2,33 @@ export const PROJECTS = [
     {
         index: 0,
         title: 'Aboutme',
-        path: '/aboutme',
-        tag: 'web',
-        roles: 'UX/UI DESIGNER, FRONTEND DEV',
-        techs: 'CSS, JS',
-        link:  'https://aboutme-pjgv.netlify.app/'
+        description: 'One page project',
+        techs: 'HTML, CSS, JS',
+        linkGithub: '',
+        linkVisitProject:  'https://aboutme-pjgv.netlify.app/'
     },
     {
         index: 1,
         title: 'Todoapp',
-        path: '/todoapp',
-        tag: 'app',
-        roles: 'UX/UI DESIGNER, FRONTEND DEV',
-        techs: 'SASS, JS',
-        link:  'https://todoapp-pjgv.netlify.app/'
+        description: 'Add, check & delete any task',
+        techs: 'HTML, SASS, JS',
+        linkGithub: 'Add, check & delete any task with this Todolist',
+        linkVisitProject:  'https://todoapp-pjgv.netlify.app/'
     },
     {
         index: 2,
         title: 'Quoteapp',
-        path: '/quoteapp',
-        tag: 'app',
-        roles: 'UX/UI DESIGNER, FRONTEND DEV',
-        techs: 'SASS, JS',
-        link:  'https://quoteapp-pjgv.netlify.app/'
+        description: 'Generate random quotes using an API',
+        techs: 'HTML, SASS, JS',
+        linkGithub: '',
+        linkVisitProject:  'https://quoteapp-pjgv.netlify.app/'
     },
     {
         index: 3,
         title: "Robberta's",
-        path: '/robbertas',
-        tag: 'web',
-        roles: 'UX/UI DESIGNER, FRONTEND DEV',
-        techs: 'SASS, JS, PHP, WORDPRESS',
-        link:  'https://pedrojgil.com/robbertas'
+        description: 'E-commerce setted up on Wordpress',
+        techs: 'HTML, SASS, JS, PHP',
+        linkGithub: '',
+        linkVisitProject:  'https://pedrojgil.com/robbertas'
     },
 ];
